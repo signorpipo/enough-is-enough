a4b4f8ca7d8c1078f6d495d990c8a50b30dd0771
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-02-10 17:55:30 +0100

    Created grab component draft

---
+++ enoughISenough/js/grab.js
@@ -0,0 +1,42 @@
+WL.registerComponent('eie-grab', {
+}, {
+    init: function() {
+        WL.canvas.addEventListener('mousedown', this.startGrab.bind(this));
+        WL.canvas.addEventListener('mouseup', this.endGrab.bind(this));
+    },
+    update: function(dt) {
+    },
+    start: function() {
+        WL.onXRSessionStart.push(this.setupVREvents.bind(this));
+    },
+    setupVREvents: function(s) {
+        this.session = s;
+        s.addEventListener('end', function(e) {
+            this.session = null;
+            this.rightGamepad = null;
+            this.leftGamepad = null;
+        }.bind(this));
+
+        s.addEventListener('inputsourceschange' ,function(e) {
+        if(e.added && e.added.length) {
+            for (var i = 0; i < e.added.length; i++) {
+            if(e.added[i].handedness == "right") {
+                this.rightGamepad = e.added[i].gamepad;
+            } else {
+                this.leftGamepad = e.added[i].gamepad;
+            }
+            }
+        }
+        }.bind(this));
+
+        s.addEventListener('selectstart', this.startGrab.bind(this));
+
+        s.addEventListener('selectend', this.endGrab.bind(this));
+    },
+    startGrab: function(e) {
+        console.log('start grab');
+    },
+    endGrab: function(e) {
+        console.log('end grab');
+    },
+});
