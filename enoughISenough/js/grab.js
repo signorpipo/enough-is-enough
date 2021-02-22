423e80b0f097490ba151b1af7c7fe1ed4a6e7f75
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-02-22 11:54:41 +0100

    Trying to get a collision callback

--- enoughISenough/js/grab.js
+++ enoughISenough/js/grab.js
@@ -3,8 +3,11 @@ WL.registerComponent('eie-grab', {
     init: function() {
         var input = this.object.getComponent("input");
 
-        MM.gamepadManager.registerStartSelect(this.startGrab, input.handedness);
-        MM.gamepadManager.registerEndSelect(this.endGrab, input.handedness);
+        MM.gamepadManager.registerStartSelect(this.startGrab.bind(this), input.handedness);
+        MM.gamepadManager.registerEndSelect(this.endGrab.bind(this), input.handedness);
+        
+        this.collider = utilities.getComponentWithChildren(this.object, 'physx', 1);
+        this.collider.onCollision(this.onCollision.bind(this));
     },
     start: function() {
     },
@@ -16,4 +19,7 @@ WL.registerComponent('eie-grab', {
     endGrab: function(e) {
         console.log('end grab');
     },
+    onCollision: function(a,b){
+        console.log('collision');
+    }
 });
