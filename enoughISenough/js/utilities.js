1a20bf5e3331d61f79a70c405d202aed7c484be5
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-02-23 16:35:21 +0100

    Reparent keep transform

--- enoughISenough/js/utilities.js
+++ enoughISenough/js/utilities.js
@@ -1,26 +1,36 @@
 var utilities = {
 
     // Negative depth means all the hierarchy
-    getComponentWithChildren: function(object, componentName, depth){
-        var objComponent = object.getComponent(componentName);
-        if(objComponent){
+    getComponentWithChildren: function (object, componentName, depth) {
+        let objComponent = object.getComponent(componentName);
+        if (objComponent) {
             return objComponent;
         }
 
-        if(depth == 0){
+        if (depth == 0) {
             return null;
         }
 
-        var children = object.children;
+        let children = object.children;
 
-        for(var i=0; i < children.length; i++){
-            objComponent = utilities.getComponentWithChildren(children[i], componentName, depth-1);
-            if(objComponent){
+        for (let i = 0; i < children.length; i++) {
+            objComponent = utilities.getComponentWithChildren(children[i], componentName, depth - 1);
+            if (objComponent) {
                 break;
             }
         }
 
         return objComponent;
+    },
+
+    reparentKeepTransform: function (object, newParent) {
+        let tempTransform = new Float32Array(8);
+
+        glMatrix.quat2.conjugate(tempTransform, newParent.transformWorld);
+        glMatrix.quat2.mul(tempTransform, tempTransform, object.transformWorld);
+
+        object.parent = newParent;
+        object.transformLocal.set(tempTransform);
     }
 
 }
