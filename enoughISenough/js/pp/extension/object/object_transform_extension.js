a714981c128b374ebba2123225ae85027725bbdc
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-07-12 15:34:09 +0200

    Get Transform, Axes

--- enoughISenough/js/pp/extension/object/object_transform_extension.js
+++ enoughISenough/js/pp/extension/object/object_transform_extension.js
@@ -1,4 +1,5 @@
 
+//GETTER
 
 //Position
 
@@ -34,6 +35,10 @@ WL.Object.prototype.pp_getRotationEulerRadians = function (rotation) {
     return this.pp_getRotationWorldEulerRadians(rotation);
 };
 
+WL.Object.prototype.pp_getRotationMatrix = function (rotation) {
+    return this.pp_getRotationWorldMatrix(rotation);
+};
+
 WL.Object.prototype.pp_getRotationQuat = function (rotation) {
     return this.pp_getRotationWorldQuat(rotation);
 };
@@ -61,6 +66,15 @@ WL.Object.prototype.pp_getRotationWorldEulerRadians = function () {
     return function (rotation = glMatrix.vec3.create()) {
         this.pp_getRotationWorldQuat(quat);
         this._pp_quaternionToEuler(quat, rotation);
+        return rotation;
+    };
+}();
+
+WL.Object.prototype.pp_getRotationWorldMatrix = function () {
+    let quat = glMatrix.quat.create();
+    return function (rotation = glMatrix.mat3.create()) {
+        this.pp_getRotationWorldQuat(quat);
+        glMatrix.mat3.fromQuat(rotation, quat);
         return rotation;
     };
 }();
@@ -97,6 +111,15 @@ WL.Object.prototype.pp_getRotationLocalEulerRadians = function () {
     };
 }();
 
+WL.Object.prototype.pp_getRotationLocalMatrix = function () {
+    let quat = glMatrix.quat.create();
+    return function (rotation = glMatrix.mat3.create()) {
+        this.pp_getRotationLocalQuat(quat);
+        glMatrix.mat3.fromQuat(rotation, quat);
+        return rotation;
+    };
+}();
+
 WL.Object.prototype.pp_getRotationLocalQuat = function (rotation = glMatrix.quat.create()) {
     glMatrix.quat.copy(rotation, this.transformLocal);
     return rotation;
@@ -117,6 +140,153 @@ WL.Object.prototype.pp_getScaleLocal = function (scale = glMatrix.vec3.create())
     glMatrix.vec3.copy(scale, this.scalingLocal);
     return scale;
 };
+
+//Transform
+
+WL.Object.prototype.pp_getTransform = function (transform) {
+    return this.pp_getTransformWorld(transform);
+};
+
+WL.Object.prototype.pp_getTransformMatrix = function (transform) {
+    return this.pp_getTransformWorldMatrix(transform);
+};
+
+WL.Object.prototype.pp_getTransformQuat = function (transform) {
+    return this.pp_getTransformWorldQuat(transform);
+};
+
+//Transform World
+
+WL.Object.prototype.pp_getTransformWorld = function (transform) {
+    return this.pp_getTransformWorldMatrix(transform);
+};
+
+WL.Object.prototype.pp_getTransformWorldMatrix = function () {
+    let transformQuat = glMatrix.quat2.create();
+    let scale = glMatrix.vec3.create();
+    return function (transform = glMatrix.mat4.create()) {
+        this.pp_getTransformWorldQuat(transformQuat);
+        this.pp_getScaleWorld(scale);
+        glMatrix.mat4.fromQuat2(transform, transformQuat);
+        glMatrix.mat4.scale(transform, transform, scale);
+        return transform;
+    };
+}();
+
+WL.Object.prototype.pp_getTransformWorldQuat = function (transform = glMatrix.quat2.create()) {
+    glMatrix.quat2.copy(transform, this.transformWorld);
+    return transform;
+};
+
+//Transform Local
+
+WL.Object.prototype.pp_getTransformLocal = function (transform) {
+    return this.pp_getTransformLocalMatrix(transform);
+};
+
+WL.Object.prototype.pp_getTransformLocalMatrix = function () {
+    let transformQuat = glMatrix.quat2.create();
+    let scale = glMatrix.vec3.create();
+    return function (transform = glMatrix.mat4.create()) {
+        this.pp_getTransformLocalQuat(transformQuat);
+        this.pp_getScaleLocal(scale);
+        glMatrix.mat4.fromQuat2(transform, transformQuat);
+        glMatrix.mat4.scale(transform, transform, scale);
+        return transform;
+    };
+}();
+
+WL.Object.prototype.pp_getTransformLocalQuat = function (transform = glMatrix.quat2.create()) {
+    glMatrix.quat2.copy(transform, this.transformLocal);
+    return transform;
+};
+
+//Forward
+
+WL.Object.prototype.pp_getForward = function (forward) {
+    return this.pp_getForwardWorld(forward);
+};
+
+WL.Object.prototype.pp_getForwardWorld = function () {
+    let rotation = glMatrix.mat3.create();
+    return function (forward = glMatrix.vec3.create()) {
+        this.pp_getRotationWorldMatrix(rotation);
+        forward[0] = rotation[6];
+        forward[1] = rotation[7];
+        forward[2] = rotation[8];
+        return forward;
+    };
+}();
+
+
+WL.Object.prototype.pp_getForwardLocal = function () {
+    let rotation = glMatrix.mat3.create();
+    return function (forward = glMatrix.vec3.create()) {
+        this.pp_getRotationLocalMatrix(rotation);
+        forward[0] = rotation[6];
+        forward[1] = rotation[7];
+        forward[2] = rotation[8];
+        return forward;
+    };
+}();
+
+//Up
+
+WL.Object.prototype.pp_getUp = function (up) {
+    return this.pp_getUpWorld(up);
+};
+
+WL.Object.prototype.pp_getUpWorld = function () {
+    let rotation = glMatrix.mat3.create();
+    return function (up = glMatrix.vec3.create()) {
+        this.pp_getRotationWorldMatrix(rotation);
+        up[0] = rotation[3];
+        up[1] = rotation[4];
+        up[2] = rotation[5];
+        return up;
+    };
+}();
+
+
+WL.Object.prototype.pp_getUpLocal = function () {
+    let rotation = glMatrix.mat3.create();
+    return function (up = glMatrix.vec3.create()) {
+        this.pp_getRotationLocalMatrix(rotation);
+        up[0] = rotation[3];
+        up[1] = rotation[4];
+        up[2] = rotation[5];
+        return up;
+    };
+}();
+
+//Right
+
+WL.Object.prototype.pp_getRight = function (right) {
+    return this.pp_getRightWorld(right);
+};
+
+WL.Object.prototype.pp_getRightWorld = function () {
+    let rotation = glMatrix.mat3.create();
+    return function (right = glMatrix.vec3.create()) {
+        this.pp_getRotationWorldMatrix(rotation);
+        right[0] = rotation[0];
+        right[1] = rotation[1];
+        right[2] = rotation[2];
+        return right;
+    };
+}();
+
+
+WL.Object.prototype.pp_getRightLocal = function () {
+    let rotation = glMatrix.mat3.create();
+    return function (right = glMatrix.vec3.create()) {
+        this.pp_getRotationLocalMatrix(rotation);
+        right[0] = rotation[0];
+        right[1] = rotation[1];
+        right[2] = rotation[2];
+        return right;
+    };
+}();
 
 //Utils
 
--- enoughISenough/js/test/test_prototype.js
+++ enoughISenough/js/test/test_prototype.js
@@ -24,29 +24,70 @@ WL.registerComponent('test-prototype', {
         this.consoleWarnFixed(this.object.pp_getRotationEuler());
         this.consoleWarnFixed(this.object.pp_getRotationEulerDegrees());
         this.consoleWarnFixed(this.object.pp_getRotationEulerRadians());
+        this.consoleWarnMatrix3Fixed(this.object.pp_getRotationMatrix());
         this.consoleWarnFixed(this.object.pp_getRotationQuat());
         console.warn("ROTATION WORLD");
         this.consoleWarnFixed(this.object.pp_getRotationWorld());
         this.consoleWarnFixed(this.object.pp_getRotationWorldEuler());
         this.consoleWarnFixed(this.object.pp_getRotationWorldEulerDegrees());
         this.consoleWarnFixed(this.object.pp_getRotationWorldEulerRadians());
+        this.consoleWarnMatrix3Fixed(this.object.pp_getRotationWorldMatrix());
         this.consoleWarnFixed(this.object.pp_getRotationWorldQuat());
         console.warn("ROTATION LOCAL");
         this.consoleWarnFixed(this.object.pp_getRotationLocal());
         this.consoleWarnFixed(this.object.pp_getRotationLocalEuler());
         this.consoleWarnFixed(this.object.pp_getRotationLocalEulerDegrees());
         this.consoleWarnFixed(this.object.pp_getRotationLocalEulerRadians());
+        this.consoleWarnMatrix3Fixed(this.object.pp_getRotationLocalMatrix());
         this.consoleWarnFixed(this.object.pp_getRotationLocalQuat());
         console.warn("SCALE");
         this.consoleWarnFixed(this.object.pp_getScale());
         this.consoleWarnFixed(this.object.pp_getScaleWorld());
         this.consoleWarnFixed(this.object.pp_getScaleLocal());
+        console.warn("TRANSFORM");
+        this.consoleWarnMatrix4Fixed(this.object.pp_getTransform());
+        this.consoleWarnMatrix4Fixed(this.object.pp_getTransformMatrix());
+        this.consoleWarnFixed(this.object.pp_getTransformQuat());
+        console.warn("TRANSFORM WORLD");
+        this.consoleWarnMatrix4Fixed(this.object.pp_getTransformWorld());
+        this.consoleWarnMatrix4Fixed(this.object.pp_getTransformWorldMatrix());
+        this.consoleWarnFixed(this.object.pp_getTransformWorldQuat());
+        console.warn("TRANSFORM LOCAL");
+        this.consoleWarnMatrix4Fixed(this.object.pp_getTransformLocal());
+        this.consoleWarnMatrix4Fixed(this.object.pp_getTransformLocalMatrix());
+        this.consoleWarnFixed(this.object.pp_getTransformLocalQuat());
+        console.warn("FORWARD");
+        this.consoleWarnFixed(this.object.pp_getForward());
+        this.consoleWarnFixed(this.object.pp_getForwardWorld());
+        this.consoleWarnFixed(this.object.pp_getForwardLocal());
+        console.warn("UP");
+        this.consoleWarnFixed(this.object.pp_getUp());
+        this.consoleWarnFixed(this.object.pp_getUpWorld());
+        this.consoleWarnFixed(this.object.pp_getUpLocal());
+        console.warn("RIGHT");
+        this.consoleWarnFixed(this.object.pp_getRight());
+        this.consoleWarnFixed(this.object.pp_getRightWorld());
+        this.consoleWarnFixed(this.object.pp_getRightLocal());
         console.warn("\nTEST END");
     },
     consoleWarnFixed(vector) {
         let result = [];
         for (let value of vector) {
             result.push(value.toFixed(4));
+        }
+        console.warn(result);
+    },
+    consoleWarnMatrix4Fixed(vector) {
+        let result = [[], [], [], []];
+        for (let i = 0; i < vector.length; i++) {
+            result[Math.floor(i / 4)].push(vector[i].toFixed(4));
+        }
+        console.warn(result);
+    },
+    consoleWarnMatrix3Fixed(vector) {
+        let result = [[], [], [], []];
+        for (let i = 0; i < vector.length; i++) {
+            result[Math.floor(i / 3)].push(vector[i].toFixed(4));
         }
         console.warn(result);
     }
