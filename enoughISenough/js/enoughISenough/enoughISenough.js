d3558754cf07e7b9c1a97ef741312d8685c19311
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-09-02 10:36:00 +0200

    Add basic prepare sfx

--- enoughISenough/js/enoughISenough/enoughISenough.js
+++ enoughISenough/js/enoughISenough/enoughISenough.js
@@ -4,10 +4,43 @@ class enoughISenough {
     }
 
     start() {
+        this.prepareSFXSetups();
+
         this._myMainFSM.start();
     }
 
     update(dt) {
         this._myMainFSM.update(dt);
     }
+
+    prepareSFXSetups() {
+        let manager = PP.AudioManager;
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/sfx/NOT_ENOUGH.wav");
+            manager.addAudioSetup(SFX_ID.NOT_ENOUGH, audioSetup);
+        }
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/sfx/blabla_1.wav");
+            manager.addAudioSetup(SFX_ID.BLABLA_1, audioSetup);
+        }
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/sfx/blabla_2.wav");
+            manager.addAudioSetup(SFX_ID.BLABLA_2, audioSetup);
+        }
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/sfx/ring_rise.wav");
+            manager.addAudioSetup(SFX_ID.RING_RISE, audioSetup);
+        }
+    }
 }
+
+SFX_ID = {
+    NOT_ENOUGH: 0,
+    BLABLA_1: 1,
+    BLABLA_2: 2,
+    RING_RISE: 3
+};
