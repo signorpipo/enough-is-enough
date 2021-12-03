7a8fa7377cd0091b58407d2b087c75618f707d93
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-12-03 13:05:39 +0100

    Add timer component

---
+++ enoughISenough/js/enoughISenough/vent/timer_component.js
@@ -0,0 +1,32 @@
+WL.registerComponent("timer", {
+    _myIsStory: { type: WL.Type.Bool, default: false },
+    _mySeconds: { type: WL.Type.Object },
+    _myMinutes: { type: WL.Type.Object },
+    _myHours: { type: WL.Type.Object },
+}, {
+    init: function () {
+    },
+    start: function () {
+        this._mySecondsTextComponent = this._mySeconds.pp_getComponent("text");
+        this._myMinutesTextComponent = this._myMinutes.pp_getComponent("text");
+        this._myHoursTextComponent = this._myHours.pp_getComponent("text");
+    },
+    update: function (dt) {
+        let time = Global.myVentDuration;
+        if (this._myIsStory) {
+            time = Global.myStoryDuration;
+        }
+
+        time = Math.floor(time);
+
+        let hours = Math.floor(time / 3600);
+        time -= hours * 3600;
+        let minutes = Math.floor(time / 60);
+        time -= minutes * 60;
+        let seconds = Math.floor(time);
+
+        this._mySecondsTextComponent.text = (seconds.toFixed(0).length < 2) ? "0".concat(seconds.toFixed(0)) : seconds.toFixed(0);
+        this._myMinutesTextComponent.text = (minutes.toFixed(0).length < 2) ? "0".concat(minutes.toFixed(0)) : minutes.toFixed(0);
+        this._myHoursTextComponent.text = (hours.toFixed(0).length < 2) ? "0".concat(hours.toFixed(0)) : hours.toFixed(0);
+    }
+});
--- enoughISenough/js/pp/tool/cauldron/components/easy_transform.js
+++ enoughISenough/js/pp/tool/cauldron/components/easy_transform.js
@@ -1,6 +1,7 @@
 WL.registerComponent("pp-easy-transform", {
     _myVariableName: { type: WL.Type.String, default: "" },
     _mySetAsDefault: { type: WL.Type.Bool, default: false },
+    _myIsLocal: { type: WL.Type.Bool, default: false },
     _myScaleAsOne: { type: WL.Type.Bool, default: true }
 }, {
     init: function () {
@@ -11,12 +12,16 @@ WL.registerComponent("pp-easy-transform", {
         }
     },
     start: function () {
-        PP.myEasyTuneVariables.add(new PP.EasyTuneEasyTransform(this._myEasyTuneVariableName, this.object.pp_getTransform(), this._myScaleAsOne));
+        PP.myEasyTuneVariables.add(new PP.EasyTuneEasyTransform(this._myEasyTuneVariableName, (this._myIsLocal ? this.object.pp_getTransformLocal() : this.object.pp_getTransformWorld()), this._myScaleAsOne));
         if (this._mySetAsDefault) {
             PP.setEasyTuneWidgetActiveVariable(this._myEasyTuneVariableName);
         }
     },
     update: function () {
-        this.object.pp_setTransform(PP.myEasyTuneVariables.get(this._myEasyTuneVariableName));
+        if (this._myIsLocal) {
+            this.object.pp_setTransformLocal(PP.myEasyTuneVariables.get(this._myEasyTuneVariableName));
+        } else {
+            this.object.pp_setTransformWorld(PP.myEasyTuneVariables.get(this._myEasyTuneVariableName));
+        }
     }
 });
