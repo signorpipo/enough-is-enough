f4bc69efbf0d07a8d8be7eb1ee21e3363a729ce7
Elia Ducceschi <eliaducceschi@hotmail.it>
2022-01-13 15:18:43 +0100

    Add pulse on grab

---
+++ enoughISenough/js/enoughISenough/cauldron/pulse_on_grab.js
@@ -0,0 +1,19 @@
+WL.registerComponent("pulse-on-grab", {
+    _myHandedness: { type: WL.Type.Enum, values: ['left', 'right'], default: 'left' },
+}, {
+    init: function () {
+    },
+    start: function () {
+        if (this._myHandedness == 0) {
+            this._myGamepad = PP.myLeftGamepad;
+        } else {
+            this._myGamepad = PP.myRightGamepad;
+        }
+
+        let grab = this.object.pp_getComponent("pp-grabber-hand");
+        grab.registerGrabEventListener(this, this._onGrab.bind(this));
+    },
+    _onGrab() {
+        this._myGamepad.pulse(0.125, 0.1);
+    },
+});
