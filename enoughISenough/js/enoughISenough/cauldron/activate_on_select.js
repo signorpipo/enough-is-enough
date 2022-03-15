4e02f6558aec4d4bcd15877e57d718f770e80f6b
Elia Ducceschi <eliaducceschi@hotmail.it>
2022-03-15 12:57:30 +0100

    Add pulse on physx touch

--- enoughISenough/js/enoughISenough/cauldron/activate_on_select.js
+++ enoughISenough/js/enoughISenough/cauldron/activate_on_select.js
@@ -1,5 +1,6 @@
 WL.registerComponent("activate-on-select", {
     _myHandedness: { type: WL.Type.Enum, values: ['left', 'right'], default: 'left' },
+    _myTrigger: { type: WL.Type.Object },
 }, {
     init: function () {
     },
@@ -12,22 +13,36 @@ WL.registerComponent("activate-on-select", {
 
         this._myPhysx = this.object.pp_getComponent("physx");
 
+        this._myTriggerPhysx = this._myTrigger.pp_getComponent("physx");
+        this._myTriggerPhysx.onCollision(this._onCollision.bind(this));
+
         this._myGamepad.registerButtonEventListener(PP.ButtonType.SELECT, PP.ButtonEvent.PRESS_START, this, this._selectPressStart.bind(this));
         this._myGamepad.registerButtonEventListener(PP.ButtonType.SELECT, PP.ButtonEvent.PRESS_END, this, this._selectPressEnd.bind(this));
 
         this._myPhysx.active = false;
+        this._myTriggerPhysx.active = false;
     },
     update() {
         if (!Global.myEnableSelectPhysx) {
             this._myPhysx.active = false;
+            this._myTriggerPhysx.active = false;
         }
     },
     _selectPressStart() {
         if (Global.myEnableSelectPhysx) {
             this._myPhysx.active = true;
+            this._myTriggerPhysx.active = true;
         }
     },
     _selectPressEnd() {
         this._myPhysx.active = false;
+        this._myTriggerPhysx.active = false;
     },
+    _onCollision(type, physx) {
+        if (type == WL.CollisionEventType.TriggerTouch) {
+            if (physx.object.pp_getComponent("evidence-component") != null) {
+                this._myGamepad.pulse(0.1, 0.1);
+            }
+        }
+    }
 });
