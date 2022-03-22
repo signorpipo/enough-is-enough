8767d045d815795214f12f32bf99ead597255365
Elia Ducceschi <eliaducceschi@hotmail.it>
2022-03-22 19:38:12 +0100

    Adjust leaderboard

--- enoughISenough/js/enoughISenough/cauldron/eie_ca_dummy_server.js
+++ enoughISenough/js/enoughISenough/cauldron/eie_ca_dummy_server.js
@@ -8,29 +8,29 @@ class EIECADummyServer {
 
         if (isAroundPlayer) {
             leaderboard = [
-                { rank: 1, displayName: "Login", score: 0 },
-                { rank: 2, displayName: "And", score: 0 },
-                { rank: 3, displayName: "Play", score: 0 },
-                { rank: 4, displayName: "On", score: 0 },
-                { rank: 5, displayName: "Construct", score: 0 },
-                { rank: 6, displayName: "Arcade", score: 0 },
-                { rank: 7, displayName: "To", score: 0 },
-                { rank: 8, displayName: "Submit", score: 0 },
-                { rank: 9, displayName: "Your", score: 0 },
-                { rank: 10, displayName: "Score", score: 0 }
+                { rank: 0, displayName: "Login", score: 0 },
+                { rank: 1, displayName: "And", score: 0 },
+                { rank: 2, displayName: "Play", score: 0 },
+                { rank: 3, displayName: "On", score: 0 },
+                { rank: 4, displayName: "Construct", score: 0 },
+                { rank: 5, displayName: "Arcade", score: 0 },
+                { rank: 6, displayName: "To", score: 0 },
+                { rank: 7, displayName: "Submit", score: 0 },
+                { rank: 8, displayName: "Your", score: 0 },
+                { rank: 9, displayName: "Score", score: 0 }
             ];
         } else {
             leaderboard = [
-                { rank: 1, displayName: "The", score: 0 },
-                { rank: 2, displayName: "Leaderboard", score: 0 },
-                { rank: 3, displayName: "Is", score: 0 },
-                { rank: 4, displayName: "Available", score: 0 },
-                { rank: 5, displayName: "Only", score: 0 },
-                { rank: 6, displayName: "When", score: 0 },
-                { rank: 7, displayName: "Playing", score: 0 },
-                { rank: 8, displayName: "On", score: 0 },
-                { rank: 9, displayName: "Construct", score: 0 },
-                { rank: 10, displayName: "Arcade", score: 0 }
+                { rank: 0, displayName: "The", score: 0 },
+                { rank: 1, displayName: "Leaderboard", score: 0 },
+                { rank: 2, displayName: "Is", score: 0 },
+                { rank: 3, displayName: "Available", score: 0 },
+                { rank: 4, displayName: "Only", score: 0 },
+                { rank: 5, displayName: "When", score: 0 },
+                { rank: 6, displayName: "Playing", score: 0 },
+                { rank: 7, displayName: "On", score: 0 },
+                { rank: 8, displayName: "Construct", score: 0 },
+                { rank: 9, displayName: "Arcade", score: 0 }
             ];
         }
 
--- enoughISenough/js/pp/cauldron/utils/ca_utils.js
+++ enoughISenough/js/pp/cauldron/utils/ca_utils.js
@@ -207,29 +207,29 @@ PP.CADummyServer = class CADummyServer {
         let leaderboard = null;
 
         if (isAroundPlayer) {
-            leaderboard = [{ rank: 7, displayName: "Player 1", score: 1000000 },
-            { rank: 8, displayName: "Player 2", score: 1000000 },
-            { rank: 9, displayName: "Player 3", score: 900000 },
-            { rank: 10, displayName: "Player 4", score: 800000 },
-            { rank: 11114, displayName: "VeryLongName_05", score: 70000000 },
-            { rank: 1111, displayName: "Player 6", score: 600000 },
-            { rank: 2222, displayName: "Player 7", score: 500000 },
-            { rank: 3333, displayName: "Player 8", score: 400000 },
-            { rank: 444, displayName: "Player 9", score: 300000 },
-            { rank: 66666, displayName: "Player 10", score: 200000 },
-            { rank: 66644, displayName: "Player 11", score: 100000 }];
+            leaderboard = [
+                { rank: 7, displayName: "Player 1", score: 1000000 },
+                { rank: 8, displayName: "Player 2", score: 1000000 },
+                { rank: 9, displayName: "Player 3", score: 900000 },
+                { rank: 10, displayName: "Player 4", score: 800000 },
+                { rank: 11111, displayName: "Player 5", score: 70000000 },
+                { rank: 22222, displayName: "VeryLongName_06", score: 600000 },
+                { rank: 33333, displayName: "Player 7", score: 500000 },
+                { rank: 44444, displayName: "Player 8", score: 400000 },
+                { rank: 55555, displayName: "Player 9", score: 300000 },
+                { rank: 66666, displayName: "Player 10", score: 200000 }];
         } else {
-            leaderboard = [{ rank: 0, displayName: "Player 1", score: 1000000 },
-            { rank: 1, displayName: "Player 2", score: 1000000 },
-            { rank: 2, displayName: "Player 3", score: 900000 },
-            { rank: 3, displayName: "Player 4", score: 800000 },
-            { rank: 4, displayName: "Player 5", score: 700000 },
-            { rank: 5, displayName: "Player 6", score: 600000 },
-            { rank: 6, displayName: "Player 7", score: 500000 },
-            { rank: 7, displayName: "Player 8", score: 400000 },
-            { rank: 8, displayName: "Player 9", score: 300000 },
-            { rank: 9, displayName: "Player 10", score: 200000 },
-            { rank: 10, displayName: "Player 11", score: 100000 }];
+            leaderboard = [
+                { rank: 0, displayName: "Player 1", score: 1000000 },
+                { rank: 1, displayName: "Player 2", score: 1000000 },
+                { rank: 2, displayName: "Player 3", score: 900000 },
+                { rank: 3, displayName: "Player 4", score: 800000 },
+                { rank: 4, displayName: "Player 5", score: 700000 },
+                { rank: 5, displayName: "Player 6", score: 600000 },
+                { rank: 6, displayName: "Player 7", score: 500000 },
+                { rank: 7, displayName: "Player 8", score: 400000 },
+                { rank: 8, displayName: "Player 9", score: 300000 },
+                { rank: 9, displayName: "Player 10", score: 200000 }];
         }
 
         while (leaderboard.length > scoresAmount) {
