b7a6d1e05883c09e7129d90d5df44519bb43e2ee
Elia Ducceschi <eliaducceschi@hotmail.it>
2022-04-05 11:17:58 +0200

    New Version

--- enoughISenough/js/enoughISenough/enoughISenough_gateway.js
+++ enoughISenough/js/enoughISenough/enoughISenough_gateway.js
@@ -34,7 +34,7 @@ WL.registerComponent("enough-IS-enough-gateway", {
     },
     start: function () {
         let version = Global.mySaveManager.loadNumber("game_version", 0);
-        Global.myGameVersion = 9;
+        Global.myGameVersion = 10;
 
         let minVersionToReset = 6;
         if (version < minVersionToReset) {
