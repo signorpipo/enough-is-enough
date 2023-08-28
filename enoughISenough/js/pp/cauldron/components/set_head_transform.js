19f3bc6c5929e6c14a81f25642c2520a32ede414
Pipo <eliaducceschi@hotmail.it>
2023-08-28 18:07:22 +0200

    Reset head transform when Non XR

--- enoughISenough/js/pp/cauldron/components/set_head_transform.js
+++ enoughISenough/js/pp/cauldron/components/set_head_transform.js
@@ -2,6 +2,8 @@ WL.registerComponent('pp-set-head-transform', {
     _myFixForward: { type: WL.Type.Bool, default: true }
 }, {
     init: function () {
+        this._myUp = [0, 1, 0];
+
         this._myHeadPose = new PP.HeadPose(this._myFixForward);
     },
     start: function () {
@@ -9,6 +11,12 @@ WL.registerComponent('pp-set-head-transform', {
     },
     update: function (dt) {
         this._myHeadPose.update(dt);
-        this.object.pp_setTransformLocalQuat(this._myHeadPose.getTransformQuat());
+
+        if (PP.XRUtils.isXRSessionActive()) {
+            this.object.pp_setTransformLocalQuat(this._myHeadPose.getTransformQuat());
+        } else {
+            this.object.pp_resetTransformLocal();
+            this.object.pp_rotateAxisLocal(180, this._myUp);
+        }
     },
 });
