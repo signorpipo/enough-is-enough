534b24754d30e0a15abbdf811af922c19d0071e3
Elia <eliaducceschi@hotmail.it>
2023-10-06 17:28:31 +0200

    Also commit saves on visibility change of the window

--- enoughISenough/js/enoughISenough/cauldron/statistics_manager.js
+++ enoughISenough/js/enoughISenough/cauldron/statistics_manager.js
@@ -7,16 +7,26 @@ class StatisticsManager {
         Global.myStatistics = new Statistics();
         Global.myStatistics.load();
 
+        this._mySaveTimer = new PP.Timer(20);
+        this._myCommitOnEndTimer = new PP.Timer(0);
+
+        Global.mySaveManager.registerClearEventListener(this, this._onClear.bind(this));
+
+        window.addEventListener('visibilitychange', function () {
+            try {
+                if (document.visibilityState != "visible") {
+                    this._onXRSessionInterrupt();
+                }
+            } catch (error) {
+                // Do Nothing
+            }
+        }.bind(this));
+
         if (WL.xrSession) {
             this._onXRSessionStart(WL.xrSession);
         }
         WL.onXRSessionStart.push(this._onXRSessionStart.bind(this));
         WL.onXRSessionEnd.push(this._onXRSessionEnd.bind(this));
-
-        this._mySaveTimer = new PP.Timer(20);
-        this._myCommitOnEndTimer = new PP.Timer(0);
-
-        Global.mySaveManager.registerClearEventListener(this, this._onClear.bind(this));
     }
 
     update(dt) {
