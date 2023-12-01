640a228ad0f231706d63bcb89b897876a55160a3
Elia <eliaducceschi@hotmail.it>
2023-12-01 16:57:52 +0100

    Add character weight to clamp leaderboard names

--- enoughISenough/js/enoughISenough/cauldron/display_leaderboard.js
+++ enoughISenough/js/enoughISenough/cauldron/display_leaderboard.js
@@ -5,6 +5,29 @@ WL.registerComponent("display-leaderboard", {
     _myScoresText: { type: WL.Type.Object },
 }, {
     init: function () {
+        this._myCharacterWeightMap = new Map();
+
+        this._myCharacterWeightMap.set("M", 1.4);
+        this._myCharacterWeightMap.set("W", 1.4);
+        this._myCharacterWeightMap.set("@", 1.4);
+        this._myCharacterWeightMap.set("m", 1.3);
+        this._myCharacterWeightMap.set("w", 1.3);
+        this._myCharacterWeightMap.set("N", 1.2);
+        this._myCharacterWeightMap.set("Q", 1.1);
+        this._myCharacterWeightMap.set("U", 1.1);
+        this._myCharacterWeightMap.set("V", 1.1);
+        this._myCharacterWeightMap.set("X", 1.1);
+
+        this._myCharacterWeightMap.set("f", 0.8);
+        this._myCharacterWeightMap.set("i", 0.8);
+        this._myCharacterWeightMap.set("j", 0.8);
+        this._myCharacterWeightMap.set("l", 0.8);
+        this._myCharacterWeightMap.set("t", 0.8);
+        this._myCharacterWeightMap.set("1", 0.8);
+        this._myCharacterWeightMap.set(" ", 0.55);
+        this._myCharacterWeightMap.set(".", 0.55);
+        this._myCharacterWeightMap.set("-", 0.55);
+
     },
     start: function () {
         this._myNamesTextComponent = this._myNamesText.pp_getComponent("text");
@@ -31,23 +54,29 @@ WL.registerComponent("display-leaderboard", {
 
         let maxRankDigit = 0;
         for (let value of leaderboard) {
-            let rank = value.rank + 1;
-            if (rank.toFixed(0).length > maxRankDigit) {
-                maxRankDigit = rank.toFixed(0).length;
+            if (value != null && value.rank != null && value.displayName != null && value.score != null) {
+                let rank = value.rank + 1;
+                if (rank.toFixed(0).length > maxRankDigit) {
+                    maxRankDigit = rank.toFixed(0).length;
+                }
             }
         }
 
         for (let value of leaderboard) {
-            let rank = value.rank + 1;
-            let fixedRank = rank.toFixed(0);
-            while (fixedRank.length < maxRankDigit) {
-                fixedRank = "0".concat(fixedRank);
+            if (value != null && value.rank != null && value.displayName != null && value.score != null) {
+                let rank = value.rank + 1;
+                let fixedRank = rank.toFixed(0);
+                while (fixedRank.length < maxRankDigit) {
+                    fixedRank = "0".concat(fixedRank);
+                }
+
+                let clampedDisplayName = this._clampDisplayName(value.displayName);
+
+                namesText = namesText.concat(fixedRank, " - ", clampedDisplayName, "\n\n");
+
+                let convertedScore = this._convertTime(value.score);
+                scoresText = scoresText.concat(convertedScore, "\n\n");
             }
-
-            namesText = namesText.concat(fixedRank, " - ", value.displayName, "\n\n");
-
-            let convertedScore = this._convertTime(value.score);
-            scoresText = scoresText.concat(convertedScore, "\n\n");
         }
 
         if (this._myNamesTextComponent != null) {
@@ -75,5 +104,31 @@ WL.registerComponent("display-leaderboard", {
         let convertedTime = hoursText.concat(":", minutesText, ":", secondsText);
 
         return convertedTime;
+    },
+    _clampDisplayName(displayName) {
+        let nameCharactersToShow = 0;
+
+        let currentWeight = 0;
+        for (let i = 0; i < displayName.length; i++) {
+            let currentCharacter = displayName[i];
+            let characterWeight = this._myCharacterWeightMap.get(currentCharacter);
+            characterWeight = characterWeight != null ? characterWeight : 1;
+
+            currentWeight += characterWeight;
+
+            if (currentWeight > 15) {
+                break;
+            }
+
+            nameCharactersToShow++;
+        }
+
+        nameCharactersToShow = Math.min(nameCharactersToShow, displayName.length);
+
+        if (nameCharactersToShow < displayName.length) {
+            return displayName.slice(0, nameCharactersToShow) + "...";
+        }
+
+        return displayName;
     }
 });
