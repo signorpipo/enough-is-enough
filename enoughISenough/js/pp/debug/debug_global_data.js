a4d7e73aaa4ab5353065694b9c0c4c0c84fb4179
Elia <eliaducceschi@hotmail.it>
2024-02-16 17:54:08 +0100

    Create tools progress

--- enoughISenough/js/pp/debug/debug_global_data.js
+++ enoughISenough/js/pp/debug/debug_global_data.js
@@ -11,8 +11,8 @@ WL.registerComponent('pp-debug-global-data', {
 
         PP.myDebugData.myCubeMesh = this._myCubeMesh;
         PP.myDebugData.mySphereMesh = this._mySphereMesh;
-        PP.myDebugData.myFlatMaterial = this._myFlatMaterial;
-        PP.myDebugData.myTextMaterial = this._myTextMaterial;
+        PP.myDebugData.myFlatMaterial = this._myFlatMaterial.clone();
+        PP.myDebugData.myTextMaterial = this._myTextMaterial.clone();
         PP.myDebugData.myHandLeft = this._myHandLeft;
         PP.myDebugData.myHandRight = this._myHandRight;
     },
@@ -25,5 +25,30 @@ PP.myDebugData = {
     myFlatMaterial: null,
     myTextMaterial: null,
     myHandLeft: null,
-    myHandRight: null
+    myHandRight: null,
+    createTools: function (showVisibilityButton = true) {
+        if (!PP.myDebugData.myHandLeft.pp_getComponent("pp-easy-tune") == null) {
+            PP.myDebugData.myHandLeft.addComponent("pp-easy-tune", {
+                _myHandedness: 1,
+                _myShowOnStart: false,
+                _myShowVisibilityButton: showVisibilityButton,
+                _myEnableAdditionalButtons: true,
+                _myEnableChangeVariableShortcut: true,
+                _myPlaneMaterial: PP.myDebugData.myFlatMaterial.clone(),
+                _myTextMaterial: PP.myDebugData.myTextMaterial.clone()
+            });
+        }
+
+        if (PP.myDebugData.myHandLeft.pp_getComponent("pp-console-vr") == null) {
+            PP.myDebugData.myHandLeft.addComponent("pp-console-vr", {
+                _myHandedness: 1,
+                _myOverrideBrowserConsole: true,
+                _myShowOnStart: false,
+                _myShowVisibilityButton: showVisibilityButton,
+                _myPulseOnNewMessage: 0,
+                _myPlaneMaterial: PP.myDebugData.myFlatMaterial.clone(),
+                _myTextMaterial: PP.myDebugData.myTextMaterial.clone()
+            });
+        }
+    }
 };
