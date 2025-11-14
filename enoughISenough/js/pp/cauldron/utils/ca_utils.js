c066012a447621e577ee7d5487b01f7701736129
Pipo <eliaducceschi@hotmail.it>
2025-11-14 11:54:41 +0100

    Revert stutter fix

--- enoughISenough/js/pp/cauldron/utils/ca_utils.js
+++ enoughISenough/js/pp/cauldron/utils/ca_utils.js
@@ -451,9 +451,7 @@ PP.CAUtils = {
         } else if (PP.CAUtils.isViverse()) {
             let leaderboardSDK = PP.CAUtils.getLeaderboardSDK();
             if (leaderboardSDK != null) {
-                return new Promise(resolve => setTimeout(resolve, 100)).then(() => {
-                    return leaderboardSDK.uploadLeaderboardScore(PP.CAUtils._myViverseAppID, [{ name: leaderboardID, value: scoreToSubmit }]);
-                }).then(function () {
+                return leaderboardSDK.uploadLeaderboardScore(PP.CAUtils._myViverseAppID, [{ name: leaderboardID, value: scoreToSubmit }]).then(function () {
                     return { scoreSubmitted: true };
                 }).catch(function (error) {
                     return { scoreSubmitted: null };
